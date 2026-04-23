c5d7d0540e5620a2f9fba892be05b1669ec2cae5
Chiragk352 <kulkarnichirag354@gmail.com>
2026-04-23 22:07:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Vitals Guard AI - Health Monitoring System")
+
+heart_rate = 75
+temperature = 36.8
+oxygen = 98
+
+print("Heart Rate:", heart_rate)
+print("Temperature:", temperature)
+print("Oxygen Level:", oxygen)
